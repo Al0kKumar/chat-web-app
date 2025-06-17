93e9bc9dd50851fb061a3e4065c25f57b3e9648c
Alok <mishraalok189381@gmail.com>
2025-06-17 19:23:46 +0530

    fixed bug in auth middleware

--- server/http/src/middlewares/authmiddleware.ts
+++ server/http/src/middlewares/authmiddleware.ts
@@ -1,30 +1,23 @@
-import { NextFunction, Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-const secret = 'alok'
+import { User } from '../types/customExpress';
 
-interface CustomRequest extends Request{
-    user?:any;
-}
+const auth = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers['authorization']?.split(' ')[1];
 
-const auth = (req: CustomRequest,res: Response, next : NextFunction) => {
-     
-    const token = req.headers['authorization']?.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ msg: 'No token provided' });
+  }
 
-    if(!token){
-        return res.status(401).json({msg:"No token provided"})
+  jwt.verify(token, process.env.JWT_SECRET_KEY || 'alok', (err, decoded) => {
+    if (err) {
+      console.error('Token verification failed:', err);
+      return res.status(403).json({ msg: 'Invalid token' });
     }
 
-
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
-        if (err) {
-            console.error('Token verification failed:', err);
-        } else {
-            console.log('Decoded token:', decoded);
-            req.user = decoded;
-            next();
-        }
-    });
-
-}
+    req.user = decoded as User; 
+    next();
+  });
+};
 
 export default auth;
